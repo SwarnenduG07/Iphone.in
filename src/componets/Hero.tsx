7a9c1aebf83709762c34baf86768d40fd587b218
Sunny <swarnendughosh05@gmail.com>
2024-06-24 22:46:37 +0530

    hero.init

--- src/componets/Hero.tsx
+++ src/componets/Hero.tsx
@@ -1,7 +1,12 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+      <section className="w-full nav-height bg-black relative">
+            <div className="h-5/6 w-full flex-col">
+            <p className="hero-title">iphone 15 Pro</p>
+                
+            </div>
+      </section>
   )
 }
 
