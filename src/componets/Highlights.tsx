3ef503e773fb0264258c2962362e54746d62e7cd
Sunny <swarnendughosh05@gmail.com>
2024-06-26 20:31:14 +0530

    bug fixed

--- src/componets/Highlights.tsx
+++ src/componets/Highlights.tsx
@@ -1,8 +1,13 @@
-
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
 
 const Highlights = () => {
+useGSAP(() => {
+    gsap.to("#title", {opacity: 1, y: 0})
+},[])
+
   return (
-   <section id="highlights" className="w-screen overflow-hidden common-paddig bg-zinc">
+   <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
           <div className="screen-max-width">
                <div>
                     <h1 id="title" className="section-heading">Get the highlights</h1>
