58ca1c3e10d0c35150b58672f9fbd690636ad003
Sunny <swarnendughosh05@gmail.com>
2024-07-05 21:23:45 +0530

    Light.tsx added

--- src/componets/ModelView.tsx
+++ src/componets/ModelView.tsx
@@ -1,8 +1,13 @@
+import { PerspectiveCamera, View } from '@react-three/drei'
 import React from 'react'
 
-const ModelView = () => {
+const ModelView = (index: number, groupRef: string,gsapType: string, controlRef: string, setLargeRotation: string, size: number, item: string) => {
   return (
-    <div>ModelView</div>
+    <View className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%]: ''`}>
+        <ambientLight intensity={0.3}/>
+
+        <PerspectiveCamera makeDefault position={[0,0,4]} />
+    </View>
   )
 }
 
